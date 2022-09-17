646381c4accc4eca35789bd80e47826d8b185cb2
davidr85 <32004282+davidr85@users.noreply.github.com>
2022-09-17 12:48:31 +0200

    Camera and http post
    Submitting first test version to be used in heroku

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {WebcamImage} from "ngx-webcam";
+import {HttpClient} from "@angular/common/http";
+import {catchError} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  // webcamImage: WebcamImage = null;
+  webcamImage: WebcamImage | undefined;
+
+  constructor(private http: HttpClient) {
+  }
+
+  handleImage(webcamImage: WebcamImage) {
+    this.webcamImage = webcamImage;
+
+    this.http.post<WebcamImage>("https://fast-hamlet-23582.herokuapp.com/", this.webcamImage).subscribe(data => {
+      console.log("Did the post request to the backend!");
+    })
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {WebcamModule} from "ngx-webcam";
+import {CameraComponent} from "../camera/camera.component";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CameraComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    WebcamModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
