e00b096a48651ba415f0a09e6b68324e6e6839fd
NinaWie <wnina@student.ethz.ch>
2022-09-17 12:58:46 +0200

    fix return bug

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def test_method():
-    jsonify("Welcome to EnergyManager")
+    return jsonify("Welcome to EnergyManager")
 
 
 @app.route("/process_floorplan", methods=["GET", "POST"])
