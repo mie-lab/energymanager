dff8030cce727660bc8dfced4d533cfb55de62b4
NinaWie <wnina@student.ethz.ch>
2022-09-17 10:52:09 +0200

    flask and heroku setup

---
+++ app.py
@@ -0,0 +1,16 @@
+import numpy as np
+from flask import Flask, jsonify, request, after_this_request
+
+from backend.process_floorplan import process_floorplan
+
+app = Flask(__name__)
+
+
+@app.route("/process_floorplan", methods=["GET"])
+def call_process_floorplan():
+    out = process_floorplan(np.random.rand(4, 5))
+    return jsonify(out)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="localhost", port=8989)
---
+++ backend/process_floorplan.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+def process_floorplan(floorplan_img):
+    return floorplan_img.shape
