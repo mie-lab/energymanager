6b10758be1cb44994a22fb90bc9c5cb2216da71a
NinaWie <wnina@student.ethz.ch>
2022-09-17 12:07:28 +0200

    enable passing an image via http request to flask

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import numpy as np
+import base64
 from flask import Flask, jsonify, request, after_this_request
 
 from backend.process_floorplan import process_floorplan
@@ -6,9 +7,24 @@ from backend.process_floorplan import process_floorplan
 app = Flask(__name__)
 
 
-@app.route("/process_floorplan", methods=["GET"])
+@app.route("/", methods=["GET"])
+def test_method():
+    jsonify("Welcome to EnergyManager")
+
+
+@app.route("/process_floorplan", methods=["GET", "POST"])
 def call_process_floorplan():
-    out = process_floorplan(np.random.rand(4, 5))
+    # get image and image shape
+    file = request.args.get("image_data")
+    shape = eval(request.args.get("image_shape"))
+    # decode
+    b64file = base64.b64decode(file)
+    img = np.fromstring(b64file, np.uint8).reshape(shape)
+
+    # run img processing
+    out = process_floorplan(img)
+
+    # return output
     return jsonify(str(out))
 
 
---
+++ tests/test_app.py
@@ -0,0 +1,16 @@
+import numpy as np
+import requests
+import ast
+import base64
+
+
+def test_post_img():
+    img = (np.random.rand(4, 5) * 256).astype(np.uint8)
+    parameters = {"image_data": base64.b64encode(img), "image_shape": str(img.shape)}
+    ret = requests.post("http://localhost:8989/process_floorplan", params=parameters)
+    print("Output shape", ret.json())
+    assert "200" in str(ret)
+
+
+if __name__ == "__main__":
+    test_post_img()
